13f13fa3f2b127830c78d229fc92e22666bcafe6
mua-wallace <muawallizewi@gmail.com>
2021-10-06 12:24:32 +0100

    implemented Home Product and Backend(using express js)

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoadingBox() {
+    return (
+        <div>
+            
+            <i className="fas fa-spinner"> Loading.....</i>
+        </div>
+    )
+}
+
+export default LoadingBox
