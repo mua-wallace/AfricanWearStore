0baf22702841b6687b52a5bf07fd80ffb62fad69
mua-wallace <muawallizewi@gmail.com>
2021-10-20 10:10:33 +0100

    implemented register Screen

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -34,6 +34,24 @@ userRouter.get('/seed',
             }
             res.status(401).send({ message: 'Invalid emai or password'});
         })
-    )
+    );
+    userRouter.post(
+        '/register',
+        expressAsyncHandler(async (req, res) =>{
+            const user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 8),
+            });
+            const createdUser = await user.save();
+            res.send({
+                        _id: createdUser._id,
+                        name: createdUser.name,
+                        email: createdUser.email,
+                        isAdmin: createdUser.isAdmin,
+                        token:generateToken(user),
+                    });
+        })
+    );
 
 export default userRouter
--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LoadingBox() {
     return (
-        <div>
+        <div className="loading">
             
             <i className="fas fa-spinner"> Loading.....</i>
         </div>
